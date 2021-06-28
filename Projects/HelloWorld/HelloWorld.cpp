a7b2019488327ba05f78bfc7a766a9af5a727b65
Babalion <48565736+Babalion@users.noreply.github.com>
2021-06-28 17:03:20 +0200

    try to implement autocorrelation with FFT, not finished yet

--- Projects/HelloWorld/HelloWorld.cpp
+++ Projects/HelloWorld/HelloWorld.cpp
@@ -1,21 +1,78 @@
 //
 // Created by chris on 26.04.21.
 //
+#include <CvPlot/cvplot.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fft_real.h>
+#include <gsl/gsl_fft_halfcomplex.h>
 #include <iostream>
-#include "HelloWorld.h"
-#include "test.h"
 
-int main(){
-    runHelloWorld();
-}
+#include "test.h"
 
 // Starts the Project Hello World. Returns 0 if no error occurs.
 int runHelloWorld() {
 
     std::cout << "HelloWorld! Es scheint alles zu funktionieren!." << std::endl;
 
-    test A=test();
-    std::cout<<A.n<<std::endl;
+    test A = test();
+    std::cout << A.n << std::endl;
 
     return 0;
+}
+
+void testFFT() {
+    int n = 3000;
+    std::vector<double> data(n);
+
+    gsl_fft_real_wavetable *real;
+    gsl_fft_halfcomplex_wavetable *hc;
+    gsl_fft_real_workspace *work;
+
+    for (int i = 0; i < n; i++) {
+        data[i] = ((double) (i % 500)) / 500;
+    }
+
+    auto axes = CvPlot::makePlotAxes();
+    auto ac=autoCorr(data);
+    normalize(ac);
+    axes.create<CvPlot::Series>(ac, "-b");
+
+    printf("Daten\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d: %e\n", i, data[i]);
+    }
+    printf("\n");
+
+    work = gsl_fft_real_workspace_alloc(n);
+    real = gsl_fft_real_wavetable_alloc(n);
+
+
+    gsl_fft_real_wavetable_free(real);
+
+    hc = gsl_fft_halfcomplex_wavetable_alloc(n);
+
+    gsl_fft_halfcomplex_inverse(&data[0], 1, n, hc, work);
+
+    data.resize(data.size()/2);
+    for (size_t i = 0; i < data.size(); ++i) {
+            data[i] /= data[0];
+    }
+
+    gsl_fft_halfcomplex_wavetable_free(hc);
+
+    printf("FFT-Daten\n");
+    for (int i = 0; i < data.size(); i++) {
+        printf("%d: %e\n", i, data[i]);
+    }
+
+    axes.create<CvPlot::Series>(data, "-r");//.setLineSpec("o");
+
+    CvPlot::show("autocorr", axes);
+
+    gsl_fft_real_workspace_free(work);
+}
+
+
+int main() {
+    testFFT();
 }
--- Projects/HelloWorld/test.h
+++ Projects/HelloWorld/test.h
@@ -1,9 +1,13 @@
 //
 // Created by chris on 07.05.21.
 //
+#pragma once
 
-#ifndef NUMERISCHEMETHODENSTATISTISCHENPHYSIK_TEST_H
-#define NUMERISCHEMETHODENSTATISTISCHENPHYSIK_TEST_H
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fft_real.h>
+#include <gsl/gsl_fft_halfcomplex.h>
+#include <numeric>
+#include <vector>
 
 
 class test {
@@ -11,5 +15,138 @@ public:
     int n;
 };
 
+/**
+ * Calculates autocorrelation with FFT
+ * @param vec
+ * @return
+ *//*
+std::vector<double> ac(const std::vector<double> &vec){
+    std::vector<double> fftReturn=vec;
 
-#endif //NUMERISCHEMETHODENSTATISTISCHENPHYSIK_TEST_H
+    gsl_fft_real_wavetable *real;
+    gsl_fft_halfcomplex_wavetable *hc;
+    gsl_fft_real_workspace *work;
+
+    //FFT
+    work = gsl_fft_real_workspace_alloc(vec.size());
+    real = gsl_fft_real_wavetable_alloc(vec.size());
+
+    gsl_fft_real_transform(&fftReturn[0], 1, vec.size(), real, work);
+    gsl_fft_real_wavetable_free(real);
+
+    hc = gsl_fft_halfcomplex_wavetable_alloc(vec.size());
+
+    gsl_fft_halfcomplex_inverse(&fftReturn[0], 1, vec.size(), hc, work);
+
+    gsl_fft_halfcomplex_wavetable_free(hc);
+    return fftReturn;
+}*/
+
+
+/**
+ * @tparam T
+ * @param vec
+ * @return maximum element of given vector
+ */
+template<typename T>
+T max(const std::vector<T> &vec) {
+    T maxVal = vec[0];
+    for (int i = 1; i < vec.size(); ++i) {
+        maxVal = std::max(maxVal, vec[i]);
+    }
+    return maxVal;
+}
+
+/**
+ * @tparam T
+ * @param vec
+ * @return minimum element of given vector
+ */
+template<typename T>
+T min(const std::vector<T> &vec) {
+    T minVal = vec[0];
+    for (int i = 1; i < vec.size(); ++i) {
+        minVal = std::min(minVal, vec[i]);
+    }
+    return minVal;
+}
+
+/**
+ * Calculates the arithmetic mean of given vector-elements
+ * @tparam T
+ * @param vec
+ * @return the arithmetic mean
+ */
+template<typename T>
+T mean(const std::vector<T> &vec) {
+    return std::accumulate(vec.begin(), vec.end(), 0.0)/vec.size();
+}
+
+/**
+ * Calculates the arithmetic mean of given vector-of-vector-of-elements
+ * @tparam T
+ * @param vec vector of vector of elements (tensor of rank 2)
+ * @return the arithmetic mean of all values
+ */
+template<typename T>
+T mean(const std::vector<std::vector<T>> &vec) {
+    std::vector<T> firstMeans;
+    firstMeans.reserve(vec.size());
+    for(auto &i:vec){
+        firstMeans.push_back(mean(i));
+    }
+    return mean(firstMeans);
+}
+
+/**
+ * Calculates the standard deviation of given vector-elements
+ * @tparam T
+ * @param vec
+ * @return standard deviation
+ */
+template<typename T>
+T stdev(const std::vector<T> &vec) {
+    T summation=0;
+    const T vec_mean=mean(vec);
+    for (size_t i = 0; i < vec.size(); ++i) {
+        summation+=pow(vec[i]-vec_mean,2);
+    }
+    return sqrt(summation*1.0/(vec.size()-1));
+}
+
+/**
+ * Calculates the autocorrelation-function of given data-sample
+ * @tparam T
+ * @param vec of data-sample with length n
+ * @return autocorrelation-function in vector with length n/2
+ */
+template<typename T>
+std::vector<T> autoCorr(const std::vector<T> &vec) {
+    float mean = std::accumulate(vec.begin(), vec.end(), 0.0f) / vec.size();
+
+    std::vector<T> autocorrelation(vec.size() / 2);
+    for (size_t t = 0; t < autocorrelation.size(); t++) {
+        float n = 0; // Numerator
+        float d = 0; // Denominator
+        for (size_t i = 0; i < vec.size() - t; i++) {
+            float xim = vec[i] - mean;
+            n += xim * (vec[(i + t) % vec.size()] - mean);
+            d += xim * xim;
+        }
+        autocorrelation[t] = n / d;
+    }
+    return autocorrelation;
+}
+
+/**
+ * normalizes given vector call by reference
+ * @tparam T
+ * @param vec vector which should be normalized
+ */
+template<typename T>
+void normalize(std::vector<T> &vec) {
+    const T vec_max = max(vec);
+    for (T &i : vec) {
+        i /= 1.0 * vec_max;
+    }
+}
